418de81984df94edae54c3459978698bcad9cae0
Philipp Geier <philipp.geier@ecmwf.int>
2024-08-26 07:52:50 +0000

    mars2grib: Add new rule that allows reading statistical param information only

--- metkit/mars2grib/GenerateTimeDescriptionRule.py
+++ metkit/mars2grib/GenerateTimeDescriptionRule.py
@@ -10,22 +10,55 @@ import sys
 
 import yaml
 from yaml.loader import SafeLoader
+import json
 
+
+def printDatabaseInformation():
+    import pymysql
+    with pymysql.connect(host="webapps-db-prod", database="param", user="ecmwf_ro", password="ecmwf_ro") as conn:
+        with conn.cursor(pymysql.cursors.DictCursor) as cur:
+            cur.execute("""DESCRIBE grib_encoding""")
+            rows = cur.fetchall()
+        
+        print(rows)
 
 
 def fetchStatisticalParamIds():
     import pymysql
     with pymysql.connect(host="webapps-db-prod", database="param", user="ecmwf_ro", password="ecmwf_ro") as conn:
         with conn.cursor(pymysql.cursors.DictCursor) as cur:
+#             cur.execute("""
+# SELECT 
+#     DISTINCT grib_encoding.param_id as paramId, 
+#              grib.attribute_value as typeOfStatisticalProcessing 
+# FROM 
+#     grib 
+#     join grib_encoding on grib_encoding.id=grib.encoding_id 
+# WHERE grib.attribute_id = (select id from attribute where name = \"typeOfStatisticalProcessing\") 
+#   and grib.attribute_value is not NULL
+# """)
             cur.execute("""
 SELECT 
     DISTINCT grib_encoding.param_id as paramId, 
-             grib.attribute_value as typeOfStatisticalProcessing 
+             grib_encoding.param_version,
+             typeOfStat.typeOfStatisticalProcessing,
+             lenTimeRange.lengthOfTimeRange,
+             unitTimeRange.indicatorOfUnitForTimeRange
 FROM 
-    grib 
-    join grib_encoding on grib_encoding.id=grib.encoding_id 
-WHERE grib.attribute_id = (select id from attribute where name = \"typeOfStatisticalProcessing\") 
-  and grib.attribute_value is not NULL
+    grib_encoding
+    INNER JOIN (SELECT encoding_id, attribute_value as typeOfStatisticalProcessing 
+        FROM grib 
+        WHERE grib.attribute_id = (select id from attribute where name = \"typeOfStatisticalProcessing\") 
+      AND grib.attribute_value is not NULL) typeOfStat 
+      ON grib_encoding.id=typeOfStat.encoding_id 
+    LEFT JOIN (SELECT encoding_id, attribute_value as lengthOfTimeRange
+        FROM grib 
+        WHERE grib.attribute_id = (select id from attribute where name = \"lengthOfTimeRange\")) lenTimeRange
+      ON grib_encoding.id=lenTimeRange.encoding_id 
+    LEFT JOIN (SELECT encoding_id, attribute_value as indicatorOfUnitForTimeRange
+        FROM grib 
+        WHERE grib.attribute_id = (select id from attribute where name = \"indicatorOfUnitForTimeRange\")) unitTimeRange
+      ON grib_encoding.id=unitTimeRange.encoding_id 
 """)
             rows = cur.fetchall()
 
@@ -38,11 +71,15 @@ WHERE grib.attribute_id = (select id from attribute where name = \"typeOfStatist
         index[key].append({k: v for k, v in row.items() if k != keyCol})
         
    
-    statisticalParamIds = []
+    statisticalParamIds = {}
     for key, vals in sorted(index.items()):
         # paramId=key, typeOfStatisticalProcessing=vals
         # Dump only paramIds as a list
-        statisticalParamIds.append(key)
+        statisticalParamIds[key] = max(vals, key = lambda d: d["param_version"])
+        del statisticalParamIds[key]["param_version"]
+        for k in list(statisticalParamIds[key].keys()):
+            if (statisticalParamIds[key][k] is None):
+                del statisticalParamIds[key][k]
     
     return statisticalParamIds
         
@@ -53,8 +90,8 @@ def fetchStatisticalParamIdsLocal(fname = "./statistical-paramids.yaml"):
 
 
 def main():
-    # statisticalParamIds = fetchStatisticalParamIds()
-    statisticalParamIds = fetchStatisticalParamIdsLocal()
+    statisticalParamIds = fetchStatisticalParamIds()
+    # statisticalParamIds = fetchStatisticalParamIdsLocal()
         
     
     # For some paramId decision is made in combiantion with stream and origin - that should be put here
@@ -79,24 +116,35 @@ def main():
                     "timeExtent": "pointInTime"
         }
     }
-    timeRange = {
+    timeRange = lambda more: {
         "write-work": {
-                    "timeExtent": "timeRange"
+                    "timeExtent": "timeRange",
+                    **more
         }
     }
+    
+    # Try to reuse existing dictionaries - allows YAML to create anchors and reduce file size
+    dictRepo = {}
+    def getDict(myDict):
+        hash = json.dumps(myDict, sort_keys=True)
+        if hash not in dictRepo.keys():
+            dictRepo[hash] = myDict
+        return dictRepo[hash]
+    
+    
     timeExtentRule = {
         "key": "paramId",
         "dict": "initial",
         "default": {
             "key": "stream",
             "dict": "initial",
-            "default": pointInTime,
+            "default": getDict(pointInTime),
             "value-map": {} # PUT in stream general stuff
         },
         "value-map": { 
-            "{}".format(paramId): (specialHandlingForParamId[paramId] 
-                if paramId in specialHandlingForParamId.keys() else timeRange
-                ) for paramId in statisticalParamIds 
+            "{}".format(paramId): (getDict(specialHandlingForParamId[paramId])
+                if paramId in specialHandlingForParamId.keys() else getDict(timeRange(more))
+                ) for paramId,more in statisticalParamIds.items()
         }
     }
     
